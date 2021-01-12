ab6e89425054816b293e50ea9538f94feff58310
19047433 <19047433@myrp.edu.sg>
2021-01-12 15:37:58 +0800

    dbfunction

--- exercise_entry.php
+++ exercise_entry.php
@@ -14,8 +14,8 @@ if (!isset($_SESSION['username'])) {
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/jquery-ui.min.css"> 
-        <link rel="stylesheet" href="css/all.css">
+         
+       
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
         <script src="js/jquery-ui.min.js" type="text/javascript"></script>
